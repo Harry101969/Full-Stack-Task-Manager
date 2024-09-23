50c519c38d78b83d115c253c2872ea5270936fe4
Harry101969 <harshagarwal19696@gmail.com>
2024-09-23 16:57:01 +0530

    Updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://full-stack-task-manager-bay.vercel.app",
         changeOrigin: true,
       },
     },
