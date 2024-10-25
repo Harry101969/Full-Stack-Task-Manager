eebefde2f05e225f8a1f8eb07d532fdca4c8140a
Harry101969 <harshagarwal19696@gmail.com>
2024-10-25 17:56:44 +0530

    ......

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -5,7 +5,6 @@ const initialState = {
   user: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
-  token: localStorage.getItem("token") || null,
 
   isSidebarOpen: false,
 };
@@ -15,20 +14,12 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setCredentials: (state, action) => {
-      // state.user = action.payload;
-      // localStorage.setItem("userInfo", JSON.stringify(action.payload));
-      const { user, token } = action.payload; // Destructure user and token from payload
-      state.user = user;
-      state.token = token; // Set token state
-      console.log(token),
-        localStorage.setItem("userInfo", JSON.stringify(user));
-      localStorage.setItem("token", token);
+      state.user = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.user = null;
-      state.token = null;
       localStorage.removeItem("userInfo");
-      localStorage.removeItem("token");
     },
     setOpenSidebar: (state, action) => {
       state.isSidebarOpen = action.payload;
