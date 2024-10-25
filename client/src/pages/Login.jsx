e9e836d562b86d67ebf695caac0261f21aefac74
Harry101969 <harshagarwal19696@gmail.com>
2024-10-25 21:28:19 +0530

    Letting loggin be the way it is

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [login, { isLoading }, refetch] = useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
   const submitHandler = async (data) => {
     try {
       const result = await login(data).unwrap();
@@ -30,7 +30,7 @@ const Login = () => {
         console.log("loggedin!");
       }, 100);
       toast.success("User Logged In Successully!");
-      navigate("/dashboard");
+      navigate("/");
       setTimeout(() => {
         console.log("Loading Data");
       }, 500);
