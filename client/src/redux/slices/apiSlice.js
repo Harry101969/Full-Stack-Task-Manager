544174304cd54743c693ed3479e54dc88dd209d1
Harry101969 <harshagarwal19696@gmail.com>
2024-10-25 19:53:42 +0530

    Updated

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Ensure the environment variables are logged correctly
-console.log("Environment Variables:", import.meta.env);
+// console.log("Environment Variables:", import.meta.env);
 
 const API_URI = import.meta.env.VITE_APP_BASE_URL;
-console.log(API_URI); // This should print the value of VITE_APP_BASE_URL
+//console.log(API_URI); // This should print the value of VITE_APP_BASE_URL
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api" });
 
